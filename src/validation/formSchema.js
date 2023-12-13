428eacf95fc0d3a27dd89e5c25187c88c37f5173
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2023-12-12 22:41:20 -0500

    m3pformschema

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,27 @@
 // Here goes the schema for the form
+import * as yup from "yup";
+
+const formSchema = yup.object().shape({
+    username: yup
+        .string()
+        .trim()
+        .required("Username is required!")
+        .min(3, "Username must be 3 characters long!"),
+    email: yup
+        .string()
+        .email("Must be a valid email address")
+        .required("You've gotta have an email"),
+    role: yup
+        .string()
+        .oneOf(['instrcutor', 'student', 'alumni'], 'Role is required!'),
+    civil: yup
+        .string()
+        .oneOf(["married", "single"], "Civil status is required!"),
+    coding: yup.boolean(),
+    reading: yup.boolean(),
+    hiking: yup.boolean(),
+
+})
+
+
+export default formSchema;
